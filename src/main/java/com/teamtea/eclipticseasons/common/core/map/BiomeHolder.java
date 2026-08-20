e4d28e868b75039a580fe03e333b20865ddfa58d
joe-vettek <my1233215@126.com>
2026-08-20 14:16:36 +0800

    Update BiomeHolder.java

--- src/main/java/com/teamtea/eclipticseasons/common/core/map/BiomeHolder.java
+++ src/main/java/com/teamtea/eclipticseasons/common/core/map/BiomeHolder.java
@@ -94,7 +94,7 @@ public class BiomeHolder {
             for (int j = 0; j < 16; j++) {
                 int xm = chunkPos.getBlockX(i);
                 int zm = chunkPos.getBlockZ(j);
-                mutableBlockPos.set(xm, chunk.getHeight(Heightmap.Types.WORLD_SURFACE_WG, i, j) + 1, zm);
+                mutableBlockPos.set(xm, chunk.getHeight(Heightmap.Types.WORLD_SURFACE, i, j) + 1, zm);
                 Holder<Biome> unCachedSurfaceBiome = MapChecker.getUnCachedSurfaceBiome(serverLevel, mutableBlockPos);
                 newBiomes[i * 16 + j] = registryUpdate ?
                         MapChecker.biomeToId(biomeRegistry, unCachedSurfaceBiome.value()) :
@@ -120,7 +120,7 @@ public class BiomeHolder {
                 if (MapChecker.isSmallBiome(biomeHolder)) {
                     int xm = chunkPos.getBlockX(i);
                     int zm = chunkPos.getBlockZ(j);
-                    mutableBlockPos.set(xm, chunk.getHeight(Heightmap.Types.WORLD_SURFACE_WG, i, j) + 1, zm);
+                    mutableBlockPos.set(xm, chunk.getHeight(Heightmap.Types.WORLD_SURFACE, i, j) + 1, zm);
 
                     newBiomes[i * 16 + j] =
                             MapChecker.biomeToId(serverLevel, MapChecker.getUnCachedSurfaceBiome(serverLevel, mutableBlockPos).value());
